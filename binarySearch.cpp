c0ab394829413ab16a8034d86ca369228134debb
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-09-18 10:51:19 +0530

    initial commit

---
+++ binarySearch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr [], int l, int r, int x) {
+    while ( l <= r ) {
+        int m = (l + r) / 2;
+        if ( arr[m] == x )
+            return m;
+        if ( arr[m] < x )
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return -1;
+}
+
+int main () {
+
+    int arr [] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int n = sizeof(arr) / sizeof(int);
+    
+    cout << binarySearch(arr, 0, n-1, 6);
+
+    return 0;
+
+}
---
+++ linearSearch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch( int arr [], int n, int x ) {
+    for ( int i = 0; i < n; i++ ) {
+        if ( arr[i] == x ) return i;
+    }
+    return -1;
+}
+
+int main () {
+
+    int arr [] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int n = sizeof(arr) / sizeof(int);
+
+    cout << linearSearch( arr, n, 6 );
+
+    return 0;
+
+}
